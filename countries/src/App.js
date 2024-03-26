7803792024b65ffdac903d255ee0a00732896737
EkaterinaPrannik <141023471+EkaterinaPrannik@users.noreply.github.com>
2024-03-26 12:32:39 +0300

    add controls: seacrh and customSelector

--- countries/src/App.js
+++ countries/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import { Container } from './components/Container';
+import Controls from './components/Controls';
 import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
   return (
     <>
-    <Header />
+      <Header />
+      <Main>
+        <Controls />
+      </Main>
     </>
   );
 }
